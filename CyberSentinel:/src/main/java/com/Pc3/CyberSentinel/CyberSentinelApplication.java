8855651bf8b6b7fe2c8b122737f600da6b597ffe
VegaHidalgoLincolnJhoel <u23304682@utp.edu.pe>
2026-07-03 01:44:24 -0500

    backend

---
+++ CyberSentinel:/src/main/java/com/Pc3/CyberSentinel/CyberSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.Pc3.CyberSentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberSentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberSentinelApplication.class, args);
+	}
+
+}
